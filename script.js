85aebf9d90b1bc3160822acb07fa362389dbc71a
J Akkermans <99044668@mydavinci.nl>
2020-09-08 21:19:49 +0200

    Pizza calculator standaard af

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-var Pizza = ['Pizza klein','Pizza Middel', 'Pizza Groot'];
-var Prijs = [5,7,13];
-var TotalPrices = [0, 0, 0];
-var clicksPizzas = [0, 0, 0];
+const klein = 5;
+const middel = 7;
+const groot = 10;
 
-function pizzaClick(id){
-    
+var countKlein = 0;
+var countMiddel = 0;
+var countGroot = 0;
+
+function pizzaKlein(){
+    if(countKlein > 0){
+        document.getElementById("pizzaKlein").innerHTML = "pizza's"
+    }
+
+    countKlein++;
+    totalKlein = countKlein*klein
+
+    document.getElementById("klein").innerHTML = countKlein;
+    document.getElementById("totalKlein").innerHTML = totalKlein + " Euro";
 }
+
+function pizzaMiddel(){
+    if(countMiddel > 0){
+        document.getElementById("pizzaMiddel").innerHTML = "pizza's"
+    }
+
+    countMiddel++;
+    totalMiddel = countMiddel*middel
+    console.log(countMiddel);
+    document.getElementById("middel").innerHTML = countMiddel;
+    document.getElementById("totalMiddel").innerHTML = totalMiddel + " Euro";
+}
+
+function pizzaGroot(){
+    countGroot++;
+    totalGroot = countGroot*groot
+    console.log(countGroot);
+    document.getElementById("groot").innerHTML = countGroot;
+    document.getElementById("totalGroot").innerHTML = totalGroot + " Euro";
+}
+
